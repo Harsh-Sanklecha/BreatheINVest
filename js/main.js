e810405f924ecd88ce500a53123958759d65b286
Diana-999 <angeldianafrancis999@gmail.com>
2021-01-08 18:34:02 +0530

    addedtimer

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const timeLeftDisplay = document.querySelector('#time-left')
+    const startBtn = document.querySelector('#startButton')
+    let timeLeft = 15
+    startBtn.addEventListener('click' ,function countDown() {
+        setInterval(function () {
+            if (timeLeft <= 0) {
+                clearInterval(timeLeft = 0)
+            }
+            timeLeftDisplay.innerHTML = timeLeft
+            timeLeft -= 1
+        }, 1000)
+    }
+    )   
+})
