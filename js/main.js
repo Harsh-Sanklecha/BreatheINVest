76b6980802c0ed29be09f45367d8a53c01ac61e6
Harsh-Sanklecha <sanklecha.harsh@gmail.com>
2021-01-25 12:10:29 +0530

    Voice Recording

--- js/main.js
+++ js/main.js
@@ -1,92 +1,3 @@
-var $ = jQuery;
-var myRecorder = {
-    objects: {
-        context: null,
-        stream: null,
-        recorder: null
-    },
-    init: function () {
-        if (null === myRecorder.objects.context) {
-            myRecorder.objects.context = new (
-                window.AudioContext || window.webkitAudioContext
-            );
-        }
-    },
-    start: function () {
-        var options = { audio: true, video: false };
-        navigator.mediaDevices.getUserMedia(options).then(function (stream) {
-            myRecorder.objects.stream = stream;
-            myRecorder.objects.recorder = new Recorder(
-                myRecorder.objects.context.createMediaStreamSource(stream),
-                { numChannels: 1 }
-            );
-            myRecorder.objects.recorder.record();
-        }).catch(function (err) { });
-    },
-    stop: function (listObject) {
-        if (null !== myRecorder.objects.stream) {
-            myRecorder.objects.stream.getAudioTracks()[0].stop();
-        }
-        if (null !== myRecorder.objects.recorder) {
-            myRecorder.objects.recorder.stop();
-
-            // Validate object
-            if (null !== listObject
-                && 'object' === typeof listObject
-                && listObject.length > 0) {
-                // Export the WAV file
-                myRecorder.objects.recorder.exportWAV(function (blob) {
-                    var url = (window.URL || window.webkitURL)
-                        .createObjectURL(blob);
-
-                    // Prepare the playback
-                    var audioObject = $('<audio controls></audio>')
-                        .attr('src', url);
-
-                    // Prepare the download link
-                    var downloadObject = $('<a>&#9660;</a>')
-                        .attr('href', url)
-                        .attr('download', new Date().toUTCString() + '.wav');
-
-                    // Wrap everything in a row
-                    var holderObject = $('<div class="row"></div>')
-                        .append(audioObject)
-                        .append(downloadObject);
-
-                        
-                    url = "blob:http://"+url
-
-                    sessionStorage.setItem("audio", url);
-
-                    // Append to the list
-                    listObject.append(holderObject);
-                });
-            }
-        }
-    }
-};
-
-// Prepare the recordings list
-var listObject = $('[data-role="recordings"]');
-
-// Prepare the record button
-$('[data-role="controls"] > button').click(function () {
-    // Initialize the recorder
-    myRecorder.init();
-
-    // Get the button state 
-    var buttonState = !!$(this).attr('data-recording');
-
-    // Toggle
-    if (!buttonState) {
-        $(this).attr('data-recording', 'true');
-        myRecorder.start();
-    } else {
-        $(this).attr('data-recording', '');
-        myRecorder.stop(listObject);
-    }
-});
-
 var d = new Date();
 
 /* Timer Logic */
@@ -97,6 +8,7 @@ function myTimer(sec) {
     timer = setInterval(function () {
         $('#timer').text(sec--);
         if (sec == 0) {
+            recorder.stop();
             clearInterval(timer);
             stops();
         }
@@ -121,10 +33,29 @@ let modelURL = 'https://teachablemachine.withgoogle.com/models/z-nWfQWiC/';
 // STEP 1: Load the model!
 
 classifier = ml5.soundClassifier(modelURL + 'model.json');
+
+
+let mic, recorder, soundFile;
+
 function setup() {
 
     classifyAudio();
     noLoop();
+
+    // create an audio in
+    mic = new p5.AudioIn();
+
+    // users must manually enable their browser microphone for recording to work properly!
+    mic.start();
+
+    // create a sound recorder
+    recorder = new p5.SoundRecorder();
+
+    // connect the mic to the recorder
+    recorder.setInput(mic);
+
+    // create an empty sound file that we will use to playback the recording
+    soundFile = new p5.SoundFile();
 }
 
 // STEP 2 classify!
@@ -205,25 +136,33 @@ function stops() {
     //}
 
     console.log("Result:" + result);
-    $(location).attr('href', result+'.html');
+    $("#timerPage").css("display","none");
+    $("#"+result).css("display","block")
+    // $(location).attr('href', result+'.html');
 }
-
 
 /* Start Button */
 $('#startButton').click(function(){
+    recorder.record(soundFile);
     myTimer(15);
     $('#startButton').css("display", "none");
     $('#stopButton').css("display","block");
 });
 
 /* Stop Button */
-$('#stopButton').click(function(){
+$('#stopButton').click(function () {
+    recorder.stop();
     myTimerStop();
     $('#stopButton').css("display", "none");
     $('#startButton').css("display","block");
 });
 
-
+function playFile() {
+    soundFile.play();
+}
+function saveFile() {
+    saveSound(soundFile, 'mySound.wav');
+}
 
 $(document).ready(function () {
     $("#reportDownload").click(function(){
